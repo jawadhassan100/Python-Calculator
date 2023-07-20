ebc4e2076ce4b92c3676875f5a94b5711296e0f1
Jawad Hassan <jawadhassankhan2001@gmail.com>
2023-07-20 20:44:02 +0500

    new color added to CTKButton

--- calculator.py
+++ calculator.py
@@ -39,16 +39,16 @@ def calculate():
 result_entry = customtkinter.CTkEntry(app,font=font1,width=250,fg_color="#000000",border_color="#000000",text_color="#FFFFFF",bg_color="#000000")
 result_entry.place(x=0,y=10)
 
-Button1 = customtkinter.CTkButton(app,command=clear,text="C",font=font1,width=50,height=2,fg_color="#b5520b",hover_color="#b5520b")
+Button1 = customtkinter.CTkButton(app,command=clear,text="C",font=font1,width=50,height=2,fg_color="#FF4500",hover_color="#FF4500")
 Button1.place(x=10,y=60)
 
-Button2 = customtkinter.CTkButton(app,command=lambda:show("%"),text="%",font=font1,width=50,height=2,fg_color="#b5520b",hover_color="#b5520b")
+Button2 = customtkinter.CTkButton(app,command=lambda:show("%"),text="%",font=font1,width=50,height=2,fg_color="#FF4500",hover_color="#FF4500")
 Button2.place(x=70,y=60)
 
-Button3 = customtkinter.CTkButton(app,command=lambda:show("/"),text="/",font=font1,width=50,height=2,fg_color="#b5520b",hover_color="#b5520b")
+Button3 = customtkinter.CTkButton(app,command=lambda:show("/"),text="/",font=font1,width=50,height=2,fg_color="#FF4500",hover_color="#FF4500")
 Button3.place(x=130,y=60)
 
-Button4 = customtkinter.CTkButton(app,command=lambda:show("*"),text="x",font=font1,width=50,height=2,fg_color="#b5520b",hover_color="#b5520b")
+Button4 = customtkinter.CTkButton(app,command=lambda:show("*"),text="x",font=font1,width=50,height=2,fg_color="#FF4500",hover_color="#FF4500")
 Button4.place(x=190,y=60)
 
 Button5 = customtkinter.CTkButton(app,command=lambda:show("7"),text="7",font=font1,width=50,height=2,fg_color="#2e2a27",hover_color="#2e2a27")
@@ -60,7 +60,7 @@ Button6.place(x=70,y=120)
 Button7 = customtkinter.CTkButton(app,command=lambda:show("9"),text="9",font=font1,width=50,height=2,fg_color="#2e2a27",hover_color="#2e2a27")
 Button7.place(x=130,y=120)
 
-Button8 = customtkinter.CTkButton(app,command=lambda:show("+"),text="+",font=font1,width=50,height=2,fg_color="#b5520b",hover_color="#b5520b")
+Button8 = customtkinter.CTkButton(app,command=lambda:show("+"),text="+",font=font1,width=50,height=2,fg_color="#FF4500",hover_color="#FF4500")
 Button8.place(x=190,y=120)
 
 Button9 = customtkinter.CTkButton(app,command=lambda:show("4"),text="4",font=font1,width=50,height=2,fg_color="#2e2a27",hover_color="#2e2a27")
@@ -72,7 +72,7 @@ Button10.place(x=70,y=180)
 Button11 = customtkinter.CTkButton(app,command=lambda:show("6"),text="6",font=font1,width=50,height=2,fg_color="#2e2a27",hover_color="#2e2a27")
 Button11.place(x=130,y=180)
 
-Button12 = customtkinter.CTkButton(app,command=lambda:show("-"),text="-",font=font1,width=50,height=2,fg_color="#b5520b",hover_color="#b5520b")
+Button12 = customtkinter.CTkButton(app,command=lambda:show("-"),text="-",font=font1,width=50,height=2,fg_color="#FF4500",hover_color="#FF4500")
 Button12.place(x=190,y=180)
 
 Button13 = customtkinter.CTkButton(app,command=lambda:show("0"),text="0",font=font1,width=50,height=2,fg_color="#2e2a27",hover_color="#2e2a27")
@@ -90,7 +90,7 @@ Button16.place(x=190,y=240)
 Button17 = customtkinter.CTkButton(app,command=lambda:show("."),text=".",font=font1,width=50,height=2,fg_color="#2e2a27",hover_color="#2e2a27")
 Button17.place(x=10,y=300)
 
-Button18 = customtkinter.CTkButton(app,command=calculate,text="=",font=font1,width=170,height=2,fg_color="#b5520b",hover_color="#b5520b",corner_radius=4)
+Button18 = customtkinter.CTkButton(app,command=calculate,text="=",font=font1,width=170,height=2,fg_color="#FF4500",hover_color="#FF4500",corner_radius=4)
 Button18.place(x=70,y=300)
 
 
